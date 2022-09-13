8207379d2f19aefd5220f3b0dc475bd2965fb227
adedamola <adesinaadedamola2@gmail.com>
2022-09-13 03:13:28 -0700

    lower case alpahbet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - A function that prints alphabet in lower case
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	return (0);
+}
+
