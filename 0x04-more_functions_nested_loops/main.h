b8867fbf343f943a2ea51b85745eb941abcb684a
Adedamola Johnson <108569020+Daloamdevs@users.noreply.github.com>
2022-09-17 15:25:24 +0100

    main.h file

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _isupper(int);
+int _isdigit(int);
+void print_numbers(void);
+int mul(int, int);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int);
+void print_diagonal(int);
+void print_space(int);
+void print_square(int);
+void print_triangle(int);
+void fizzbuzz(void);
+void print_number(int);
+
+#endif
