3717592a4e2db568d1ea42837de8adcc78807c8f
Adedamola Johnson <108569020+Daloamdevs@users.noreply.github.com>
2022-09-26 22:01:56 +0100

    Memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,28 +1,20 @@
 #include "main.h"
-/**
- *_strncat - function that concatenates two strings. it will use
- * at most n bytes from src. src does not need to be null terminated.
- *@dest: pointer to destination string.
- *@src: pointer to source string.
- *@n: number of bytes to be concatenated.
- *
- *Return: pointer to destination string.
- */
-char *_strncat(char *dest, char *src, int n)
-{
-	int length, j;
-/* j is a counter for  n bytes of src to be concatenated */
-/* length = length of destination string */
 
-	length = 0;
-	while (dest[length] != '\0')
+/**
+ * _memcpy - copies memory area
+ * @dest: destination of copy
+ * @src: memory area to copy
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
 	{
-		length++;
+		*(dest + i) = *(src + i);
 	}
-	for (j = 0; j < n && src[j] != '\0'; j++, length++)
-	{
-		dest[length] = src[j];
-	}
-	dest[length] = '\0';
 	return (dest);
 }
