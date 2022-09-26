f558cd4ab2bf4a088e198489b09fc05d95cc1374
Adedamola Johnson <108569020+Daloamdevs@users.noreply.github.com>
2022-09-26 21:53:23 +0100

    Updated

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,24 @@
-#include "main.h" 
- /** 
-  *_memset - fills memory with a constant byte. 
-  *fills the first n bytes of the memory area pointed 
-  *to by s with the constant b. 
-  *@s: pointer to memory area. 
-  *@b: constant to fill memory with. 
-  *@n: bytes of the memory area to be filled. 
-  * 
-  *Return: pointer to memory area s. 
- */ 
- char *_memset(char *s, char b, unsigned int n) 
- { 
-         unsigned int counter; 
-  
-         for (counter = 0; counter < n; counter++) 
-         { 
-                 s[counter] = b; 
-         } 
-         return (s); 
- }
+#include "main.h"
+/**
+ *_strcat - function that concatenates two strings.
+ *@dest: pointer to destination string.
+ *@src: pointer to source string.
+ *
+ *Return: pointer to destination string.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int length, j; /* j will hold the length of the source */
+
+	length = 0;
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
+	for (j = 0; src[j] != '\0'; j++, length++)
+	{
+		dest[length] = src[j];
+	}
+	dest[length] = '\0';
+	return (dest);
+}
