e1eab16e3d8caedccb1896690e5b02432a4b1b4f
adedamola <adesinaadedamola2@gmail.com>
2022-09-30 10:46:06 -0700

    static library done

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - function that shows the length of a string
+ * @s: Char input
+ *
+ * Return: length of the input string
+ */
+int _strlen(char *s)
+{
+	int i = 1, sum = 0;
+	char pl = s[0];
+
+	while (pl != '\0')
+	{
+		sum++;
+		pl = s[i++];
+	}
+	return (sum);
+}
